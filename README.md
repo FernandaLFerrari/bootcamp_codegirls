976dda028945a49783c7734d88b50b4ec90454aa
FernandaLFerrari <ferrari.fernandal@gmail.com>
2022-07-19 13:26:44 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bootcamp_codegirls
+Repositório criado para armazenamento dos códigos gerados no Santander Bootcamp 2022.
---
+++ projects_java/Model/gato.java
@@ -0,0 +1,17 @@
+package Bootcamp_CodeGirls.projects_java.Model;
+
+public class gato {
+    private String nome;
+    private String cor;
+    private String idade;
+
+
+    public gato(){}
+
+    public gato(String nome, String cor, String idade){
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+
+    }
+}
