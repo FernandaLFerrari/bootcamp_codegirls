27a086008ffca135da0dd20898d9bb1f1bbfcd09
FernandaLFerrari <ferrari.fernandal@gmail.com>
2022-08-07 18:26:09 -0300

    update files  and exercise one

--- projects_java/PrimeiroPrograma.java
+++
@@ -1,8 +0,0 @@
-package Bootcamp_CodeGirls.projects_java;
-
-public class PrimeiroPrograma {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
-
---
+++ projects_java/src/main.java
@@ -0,0 +1,31 @@
+package Bootcamp_CodeGirls.projects_java.Model;
+
+/**
+ * Exercício 1
+ * Variáveis, Tipos de dados, Operadores  
+ * Criação de variáveis locais
+ * main é o ponto inicial para construção de métodos no java
+ */
+
+
+
+ public class main {
+
+    public static void main(String[] args) {
+
+        int quantidadeProduto = 50;
+        int QuantidadeProduto = 10; // não é uma boa pratica
+        final int NUMERO_TENTIVAS = 5;
+        int QUANTIDADE_OPCOES = 25; // não é uma boa pratica, somente variaveis final em letras maiusculas e com _
+
+        System.out.println(quantidadeProduto);
+        System.out.println(QuantidadeProduto);
+        System.out.println(NUMERO_TENTIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+
+   
+
+}
